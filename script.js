bf2451936eb5b0dbdc94140aea8a00574043e9e4
Edson Pereira <ed.1206@gmail.com>
2023-02-12 02:49:46 -0300

    refactor: :recycle: Code refactor

--- script.js
+++ script.js
@@ -1,49 +1,91 @@
-// Função para criar o quadrado de pixels:
-function makePixelSquares() {
-  const size = 5;
-  const frame = document.getElementById('pixel-board');
-  for (let index = 1; index <= size; index += 1) {
-    const line = document.createElement('div');
-    frame.appendChild(line);
-    for (let index2 = 1; index2 <= size; index2 += 1) {
-      const pixel = document.createElement('div');
-      pixel.classList.add('pixel');
-      line.appendChild(pixel);
-    }
-  }
-}
-makePixelSquares();
+const colorRandomOne = document.querySelector('#random-color-1');
+const colorRandomTwo = document.querySelector('#random-color-2');
+const colorRandomThree = document.querySelector('#random-color-3');
+const addColorPixel = document.querySelectorAll('.pixel');
+const palette = document.querySelectorAll('.color');
+const clearBoard = document.querySelector('#clear-board');
+const colorRandomButton = document.querySelector('#color-random-button');
+const board = document.querySelector('#pixel-board');
+const createBoardButton = document.querySelector('#generate-board');
+const inputBox = document.querySelector('#board-size');
 
 // Função para "pegar" a cor selecionada na palheta:
-const palette = document.getElementsByClassName('color');
 
 function selectColor(origin) {
-  for (let index = 0; index < palette.length; index += 1) {
-    palette[index].classList.remove('selected');
-  }
+  palette.forEach((e) => e.classList.remove('selected'));
+
   origin.target.classList.add('selected');
 }
 
-for (let index = 0; index < palette.length; index += 1) {
-  palette[index].addEventListener('click', selectColor);
-}
+palette.forEach((e) => e.addEventListener('click', selectColor));
 
 // Função para "pintar" o pixel com a cor selecionada:
-const addColorPixel = document.getElementsByClassName('pixel');
 
 function selectPixel(origin) {
-  origin.target.style.backgroundColor = window.getComputedStyle(document.querySelector('.selected'), null).getPropertyValue('background-color');
+  const { target } = origin;
+  target.style.backgroundColor = window.getComputedStyle(document.querySelector('.selected'), null)
+    .getPropertyValue('background-color');
 }
 
-for (let index = 0; index < addColorPixel.length; index += 1) {
-  addColorPixel[index].addEventListener('click', selectPixel);
-}
+addColorPixel.forEach((e) => e.addEventListener('click', selectPixel));
 
-// Função para fazer o botão de reset dos pixels:
+// Evento do botão de reset dos pixels:
 
 function cleanPixels() {
-  for (let index = 0; index < addColorPixel.length; index += 1) {
-    addColorPixel[index].style.backgroundColor = 'white';
+  document.querySelectorAll('.pixel').forEach((e) => {
+    e.style.backgroundColor = 'white';
+  });
+}
+
+clearBoard.addEventListener('click', () => {
+  cleanPixels();
+});
+
+// Função para gerar cores aleatórias:
+
+function generateRandomColor() {
+  const r = () => Math.floor(Math.random() * 256);
+  const color = `rgb(${r()}, ${r()}, ${r()})`;
+  return color;
+}
+
+const colors = [colorRandomOne, colorRandomTwo, colorRandomThree];
+
+colors.forEach((e) => { e.style.backgroundColor = generateRandomColor(); });
+
+colorRandomButton.addEventListener('click', () => {
+  colors.forEach((e) => { e.style.backgroundColor = generateRandomColor(); });
+});
+
+// Função que gera o quadro de pixels com tamanho definido pelo usuário:
+
+function buildRow(number) {
+  const divRow = document.createElement('div');
+  divRow.className = 'row';
+  for (let index = 0; index < number; index += 1) {
+    const pixels = document.createElement('div');
+    pixels.className = 'pixel';
+    divRow.appendChild(pixels);
+    pixels.addEventListener('click', selectPixel);
+  }
+  board.appendChild(divRow);
+}
+
+function buildBoard(number) {
+  board.innerHTML = '';
+  for (let index = 0; index < number; index += 1) {
+    buildRow(number);
   }
 }
-cleanPixels();
+
+// Evento do botão que cria o quadro de pixels:
+
+createBoardButton.addEventListener('click', () => {
+  let number = 0;
+  if (inputBox.value === '') {
+    alert('Board inválido!');
+  } else {
+    number = Math.min(Math.max(inputBox.value, 5), 50);
+    buildBoard(number);
+  }
+});
